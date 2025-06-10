e2b747678135eeb355bb92848c84005379394af9
klimes888 <klimes888@gmail.com>
2025-06-10 19:43:58 +0900

    fix2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type React from "react";
 import type { Metadata } from "next";
 import StyledComponentsRegistry from "@/lib/registry";
-
+const img =
+  "https://nimage.g-enews.com/phpwas/restmb_allidxmake.php?idx=5&simg=2024012318351303239c5fa75ef8612254575.jpg";
 export const metadata: Metadata = {
   title: "Blue archive!",
   description: "학원, 학생, 그리고 당신의. 기적 같은 이야기. 기적 같은 일상",
@@ -12,7 +13,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/shiroko.jpeg",
+        url: img,
         alt: "image",
       },
     ],
@@ -23,7 +24,7 @@ export const metadata: Metadata = {
     description: "학원, 학생, 그리고 당신의. 기적 같은 이야기. 기적 같은 일상",
     images: [
       {
-        url: "/shiroko.jpeg",
+        url: img,
         alt: "Image",
       },
     ],
